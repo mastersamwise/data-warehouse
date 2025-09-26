1e326910887127da8664f195cc4d5c90a138f73c
nik <nikb248@gmail.com>
2025-09-25 23:53:50 -0600

    trying to fix sql error 63

--- DataWarehouse/DataWarehouse/Library/Classes/Common/Constants.cs
+++ DataWarehouse/DataWarehouse/Library/Classes/Common/Constants.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 namespace DataWarehouse.Library.Classes.Common
 {
     public class Constants
-    {
-        public static string DB_CONNECTION_NAS = "server=10.160.0.123;port=3306;user=nbourne;password=Sam4wise8!;database=nbourne;";
+    {   
+        /// <summary>
+        /// 
+        /// </summary>
+        private static string CHAR_SET = "charset=utf8mb4;";
+        public static string DB_CONNECTION_NAS = $"server=10.160.0.123;port=3306;user=nbourne;password=Sam4wise8!;database=nbourne;{CHAR_SET}";
         
     }
 }
--- DataWarehouse/DataWarehouse/Library/Managers/PokemonManager.cs
+++ DataWarehouse/DataWarehouse/Library/Managers/PokemonManager.cs
@@ -22,7 +22,20 @@ namespace DataWarehouse.Library.Managers
             List<PokemonEvent> events = new List<PokemonEvent>();
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
-                string query = "SELECT event_name FROM pokemon_events;";
+                string query = @"SELECT event_id,
+                                        is_event_active,
+                                        event_name,
+                                        event_type,
+                                        start_date,
+                                        end_date,
+                                        serial_code,
+                                        description,
+                                        created_date,
+                                        created_by,
+                                        updated_date,
+                                        updated_by,
+                                        is_deleted
+                                 FROM pokemon_events;";
                 MySqlCommand command = new MySqlCommand(query, connection);
 
                 try
@@ -31,19 +44,19 @@ namespace DataWarehouse.Library.Managers
                     MySqlDataReader reader = command.ExecuteReader();
                     while (reader.Read())
                     {
-                        int eventID = int.Parse(reader.GetString("event_id"));
+                        int eventID = reader.GetInt32("event_id");
                         bool isEventActive = bool.Parse(reader.GetString("is_event_active"));
                         string eventName = reader.GetString("event_name");
                         string eventType = reader.GetString("event_type");
-                        DateTime startDate = DateTime.Parse(reader.GetString("start_date"));
-                        DateTime endDate = DateTime.Parse(reader.GetString("end_date"));
-                        string serialCode = reader.GetString("serial_code");
-                        string description = reader.GetString("description");
-                        DateTime createdDate = DateTime.Parse(reader.GetString("created_date"));
-                        string createdBy = reader.GetString("created_by");
-                        DateTime updatedDate = DateTime.Parse(reader.GetString("updated_date"));
-                        string updatedBy = reader.GetString("updated_by");
-                        bool isDeleted = bool.Parse(reader.GetString("is_deleted"));
+                        DateTime startDate = reader.GetDateTime("start_date");
+                        DateTime endDate = reader.GetDateTime("end_date");
+                        string serialCode = reader.IsDBNull(reader.GetOrdinal("serial_code")) ? string.Empty : reader.GetString("serial_code");
+                        string description = reader.GetString("description") ?? "";
+                        DateTime createdDate = reader.GetDateTime("created_date");
+                        string createdBy = reader.GetString("created_by") ?? "";
+                        DateTime updatedDate = reader.GetDateTime("updated_date");
+                        string updatedBy = reader.IsDBNull(reader.GetOrdinal("updated_by")) ? string.Empty : reader.GetString("updated_by");
+                        bool isDeleted = reader.GetBoolean("is_deleted");
 
                         AuditInfo tempAuditInfo = new(createdDate, createdBy, updatedDate, updatedBy, isDeleted);
                         PokemonEvent tempEvent = new(eventID, isEventActive, eventName, eventType, startDate, endDate, serialCode, description, tempAuditInfo);
