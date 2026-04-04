563ddce0ff0cd30794301729e7a848c4670f9cc1
mastersamwise <nikb248@gmail.com>
2026-04-04 15:04:23 -0600

    making a line in the sand incase gemini screws up my files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-node server.js &  # Start Node in the background
-dotnet DataWarehouse.dll # Start .NET in the foreground
+#!/bin/sh
+node /app/server.js &  # Start Node in the background
+dotnet app/DataWarehouse.dll # Start .NET in the foreground
