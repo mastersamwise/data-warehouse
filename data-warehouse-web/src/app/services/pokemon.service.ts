31adfd9515650d8c52fa41499b81c8bab77037c3
nik <nikb248@gmail.com>
2026-04-12 00:31:31 -0600

    Added the rest of CRUD functionality for pokemon event

--- data-warehouse-web/src/app/services/pokemon.service.ts
+++ data-warehouse-web/src/app/services/pokemon.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { PokemonEvent } from '../../classes/Pokemon/PokemonEvent';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
+  private url: string = 'https://localhost:4208/Pokemon';
+
   constructor(private http: HttpClient) { }
 
   getPokemonEvents(): Observable<any> {
-    return this.http.get('https://localhost:4208/Pokemon/GetPokemonEvents');
+    return this.http.get(`${this.url}/GetPokemonEvents`);
+  }
+
+  updatePokemonEvent(pokemonEvent: PokemonEvent): Observable<any> {
+    return this.http.post(`${this.url}/UpdatePokemonEvent`, pokemonEvent);
+  }
+
+  addPokemonEvent(pokemonEvent: PokemonEvent): Observable<any> {
+    return this.http.post(`${this.url}/AddPokemonEvent`, pokemonEvent);
+  }
+
+  deletePokemonEvent(eventID: number): Observable<any> {
+    return this.http.post(`${this.url}/DeletePokemonEvent`, eventID);
   }
 }
