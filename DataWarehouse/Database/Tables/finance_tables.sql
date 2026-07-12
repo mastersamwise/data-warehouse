ca33e133f423c745ec183d0ca6f609f25dc0fa97
nik <nikb248@gmail.com>
2026-07-12 00:24:15 -0600

    getting code to build and run

--- DataWarehouse/Database/Tables/finance_tables.sql
+++ DataWarehouse/Database/Tables/finance_tables.sql
@@ -1,18 +1,63 @@
+/***************************************************************************************
+*       Finance Categories
+***************************************************************************************/
 CREATE TABLE finance_categories
 (
     category_id             INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
     category_name           VARCHAR(100)    NOT NULL
 );
 
+/***************************************************************************************
+*       Accounts
+***************************************************************************************/
 CREATE TABLE finance_accounts
 (
     account_id              INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
     account_name            VARCHAR(100)    NOT NULL
 );
 
+/***************************************************************************************
+*       Orders / Payments
+***************************************************************************************/
+CREATE TABLE finance_orders
+(
+    id                      INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    date_of_payment         DATE            NOT NULL    DEFAULT '1900-01-01',
+    comment                 VARCHAR(500)    NULL,
+    created_date            DATE            NOT NULL,
+    created_by              VARCHAR(200)    NOT NULL,
+    updated_date            DATE            NULL,
+    updated_by              VARCHAR(200)    NULL,
+    is_deleted              BIT             NOT NULL    DEFAULT 0
+);
+
+/***************************************************************************************
+*       Confirmation Numbers
+***************************************************************************************/
 CREATE TABLE finance_confirmation_numbers
 (
-    confirmation_number_id  INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id                      INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    date_of_payment         DATE            NOT NULL    DEFAULT '1900-01-01',
+    date_of_arrival         DATE            NULL,
+    recipient               VARCHAR(200)    NOT NULL,
+    category_id             INT             NULL,
+    account_used_id         INT             NULL,
+    amount                  DECIMAL         NULL,
+    confirmation_number     VARCHAR(100)    NULL,
+    comment                 VARCHAR(500)    NULL,
+    created_date            DATE            NOT NULL,
+    created_by              VARCHAR(200)    NOT NULL,
+    updated_date            DATE            NULL,
+    updated_by              VARCHAR(200)    NULL,
+    is_deleted              BIT             NOT NULL    DEFAULT 0
+);
+
+/***************************************************************************************
+*       Confirmation Numbers Raw
+***************************************************************************************/
+CREATE TABLE finance_confirmation_numbers_raw
+(
+    id                      INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
     date_of_payment         DATE            NOT NULL    DEFAULT '1900-01-01',
     date_of_arrival         DATE            NULL,
     recipient               VARCHAR(200)    NOT NULL,
--- DataWarehouse/Database/Tables/pokemon_tables.sql
+++ DataWarehouse/Database/Tables/pokemon_tables.sql
@@ -1,3 +1,6 @@
+/***************************************************************************************
+*       Pokemon Events
+***************************************************************************************/
 CREATE TABLE pokemon_events 
 (
     event_id            INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -16,5 +19,119 @@ CREATE TABLE pokemon_events
     updated_by          VARCHAR(50)     NULL,
     is_deleted          BOOLEAN         DEFAULT FALSE
 );
-
 ALTER TABLE pokemon_events ADD CONSTRAINT C_U_EVENT UNIQUE (event_name, start_date, serial_code);
+
+/***************************************************************************************
+*       Pokemon Species
+***************************************************************************************/
+CREATE TABLE pokemon_species
+(
+    id                      INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    species_name            VARCHAR(50)     NOT NULL,
+    national_dex_number     INT             NOT NULL,
+    has_alt_form            BIT             DEFAULT FALSE,
+    description             VARCHAR(250)    NULL,
+    comment                 VARCHAR(500)    NULL,
+    created_date            TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    created_by              VARCHAR(50)     NULL,
+    updated_date            TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    updated_by              VARCHAR(50)     NULL,
+    is_deleted              BOOLEAN         DEFAULT FALSE
+);
+ALTER TABLE pokemon_species ADD CONSTRAINT c_u_pokemon_species UNIQUE (event_name, start_date, serial_code);
+
+/***************************************************************************************
+*       Alternate Pokemon Forms
+***************************************************************************************/
+CREATE TABLES alternate_pokemon_forms
+(
+    id                      INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    form_name               VARCHAR(100)    NOT NULL,
+    species_id              INT             NOT NULL,
+    comment                 VARCHAR(500)    NULL,
+    created_date            TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    created_by              VARCHAR(50)     NULL,
+    updated_date            TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    updated_by              VARCHAR(50)     NULL,
+    is_deleted              BOOLEAN         DEFAULT FALSE
+);
+ALTER TABLE alternate_pokemon_forms ADD CONSTRAINT c_u_alternate_pokemon_forms UNIQUE (form_name);
+
+/***************************************************************************************
+*       Pokemon Games
+***************************************************************************************/
+CREATE TABLE pokemon_games
+(
+    id                  INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    game_name           VARCHAR(50)     NOT NULL,
+    platform            VARCHAR(50)     NOT NULL,
+    release_date        TIMESTAMP       NULL,
+    comment             VARCHAR(500)    NULL,
+    created_date        TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    created_by          VARCHAR(50)     NULL,
+    updated_date        TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    updated_by          VARCHAR(50)     NULL,
+    is_deleted          BOOLEAN         DEFAULT FALSE
+);
+ALTER TABLE pokemon_games ADD CONSTRAINT c_u_pokemon_games UNIQUE (game_name);
+
+/***************************************************************************************
+*       Pokemon with Applicable Games
+***************************************************************************************/
+CREATE TABLE pokemon_with_applicable_games
+(
+    species_id          INT             NOT NULL,
+    game_id             INT             NOT NULL,
+    is_in_game          BIT             DEFAULT FALSE,
+    comment             VARCHAR(500)    NULL,
+    created_date        TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    created_by          VARCHAR(50)     NULL,
+    updated_date        TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    updated_by          VARCHAR(50)     NULL,
+    is_deleted          BOOLEAN         DEFAULT FALSE
+);
+ALTER TABLE pokemon_with_applicable_games ADD CONSTRAINT c_u_pokemon_with_applicable_games UNIQUE (species_id, game_id);
+
+/***************************************************************************************
+*       National Dex Checklist
+***************************************************************************************/
+CREATE TABLE national_dex_checklist
+(
+    id                          INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    species_id                  INT             NOT NULL,
+    is_registered_in_home       BIT             DEFAULT FALSE,
+    is_in_living_dex            BIT             DEFAULT FALSE,
+    is_shiny_in_living_dex      BIT             DEFAULT FALSE,
+    has_hidden_ability_in_home  BIT             DEFAULT FALSE,
+    comment                     VARCHAR(1000)   NULL,
+    created_date                TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    created_by                  VARCHAR(50)     NULL,
+    updated_date                TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    updated_by                  VARCHAR(50)     NULL,
+    is_deleted                  BOOLEAN         DEFAULT FALSE
+);
+
+/***************************************************************************************
+*       National Dex Checklist Raw Import
+***************************************************************************************/
+CREATE TABLE national_dex_checklist_raw_import
+(
+    id                          INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    pokemon_name                VARCHAR(100)    NOT NULL,
+    is_registered_in_home       VARCHAR(100),
+    is_in_living_dex            VARCHAR(100),
+    is_shiny_in_living_dex      VARCHAR(100),
+    has_hidden_ability_in_home  VARCHAR(100),
+    is_pokemon_in_lgfr          VARCHAR(100),
+    is_pokemon_in_swsh          VARCHAR(100),
+    is_pokemon_in_bdsp          VARCHAR(100),
+    is_pokemon_in_arceus        VARCHAR(100),
+    is_pokemon_in_sv            VARCHAR(100),
+    is_pokemon_in_za            VARCHAR(100),
+    comment                     VARCHAR(1000)   NULL,
+    created_date                TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    created_by                  VARCHAR(50)     NULL,
+    updated_date                TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    updated_by                  VARCHAR(50)     NULL,
+    is_deleted                  BOOLEAN         DEFAULT FALSE
+);
